0a077f624540777dc63820790a23bd8051bbaaec
Carlos Lubbers <carlos.lubbers@deltares.nl>
2022-09-28 13:55:59 +0200

    Test

--- tests/models/dstability/test_dstability_model.py
+++ tests/models/dstability/test_dstability_model.py
@@ -114,7 +114,7 @@ class TestDStabilityModel:
             pytest.param("dstability/Tutorial_v20_2_1", id="Tutorial DStability 20.2.1"),
             pytest.param("dstability/Tutorial_v2022_1_1", id="Tutorial DStability 2022.01"),
             pytest.param("dstability/ResultExample", id="Result Example"),
-            pytest.param("dstability/EmptyFile", id="EmptyFile")
+            pytest.param("dstability/EmptyFile", id="Empty File")
         ],
     )
     def test_execute_model_succesfully(self, dir_path: str):
