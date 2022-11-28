7320a06edaf14cb6038279cab50ecc915bf89490
JohnBokma <80911370+JohnBokma@users.noreply.github.com>
2022-11-28 15:26:35 +0100

    Added setting default value for soilcu as 0.00 and added test for it.

--- tests/soils/test_soil_bugs.py
+++ tests/soils/test_soil_bugs.py
@@ -2,7 +2,8 @@ import pytest
 from pydantic import ValidationError
 from teamcity import is_running_under_teamcity
 
-from geolib.soils import MohrCoulombParameters, Soil
+from geolib.models.dfoundations.dfoundations_model import DFoundationsModel
+from geolib.soils import MohrCoulombParameters, Soil, SoilType
 
 
 class TestNaNSoils:
@@ -23,3 +24,38 @@ class TestNaNSoils:
         with pytest.raises(ValidationError):
             kwargs = {"cohesion": 5, "friction_angle": "inf"}
             soil = Soil(mohr_coulomb_parameters=kwargs)
+
+        with pytest.raises(ValidationError):
+            kwargs = {"cohesion": 5, "friction_angle": "inf"}
+            soil = Soil(DSeriesUnmappedNameProperties=kwargs)
+
+           #from geolib.models.dfoundations.dfoundations_model import DFoundationsModel
+           #from geolib.soils.soil import Soil, SoilType
+
+    @pytest.mark.unittest
+    def test_set_Cu_Is_Optional(self):
+            #soil = Soil()
+            SOIL_PARAMETERS = [
+                {'materiaal': 'Onbekend materiaal', "unsat": 0.0, "sat": 0.0, "phi": 0.0, "soiltype": SoilType.GRAVEL},
+                {'materiaal': 'Grond, fijn korrelig', "unsat": 18.0, "sat": 20.0, "phi": 25.0, "soiltype": SoilType.SAND},
+                {'materiaal': 'Veen', "unsat": 12.0, "sat": 12.0, "phi": 15.0, "soiltype": SoilType.PEAT},
+                {'materiaal': 'Klei, zwk siltig-siltig', "unsat": 15.0, "sat": 15.0, "phi": 20.0, "soiltype": SoilType.CLAY},
+                {'materiaal': 'Klei, siltig/leem', "unsat": 16.5, "sat": 16.5, "phi": 25.0, "soiltype": SoilType.CLAY},
+                {'materiaal': 'Zand, siltig tot leem', "unsat": 17.0, "sat": 19.0, "phi": 27.5, "soiltype": SoilType.SAND},
+                {'materiaal': 'Zand, zwk siltig-siltig', "unsat": 18.0, "sat": 20.0, "phi": 30, "soiltype": SoilType.SAND},
+                {'materiaal': 'Zand, zand tot grindig', "unsat": 18.0, "sat": 20.0, "phi": 32.5, "soiltype": SoilType.GRAVEL},
+                {'materiaal': 'Zand, vastzand, kleiig', "unsat": 18.0, "sat": 20.0, "phi": 30.0, "soiltype": SoilType.SAND},
+                {'materiaal': 'Grond, zeer stijf', "unsat": 18.0, "sat": 20.0, "phi": 30.0, "soiltype": SoilType.SAND}
+            ]
+
+            model = DFoundationsModel()
+
+            for soil in SOIL_PARAMETERS:
+                soil_type = Soil()
+                soil_type.name = soil["materiaal"]
+                soil_type.soil_type_nl = soil["soiltype"]
+                soil_type.mohr_coulomb_parameters.friction_angle = soil["phi"]
+                soil_type.soil_weight_parameters.saturated_weight = soil["sat"]
+                soil_type.soil_weight_parameters.unsaturated_weight = soil["unsat"]
+                model.add_soil(soil_type)
+                assert model.soils[soil_type.name].soilcu == 0.00, "Should have default Cu with 0"
