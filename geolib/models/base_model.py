ee86d07825b1f037510c88b50dcac0549bdc4a82
Carlos Lubbers <carlos.lubbers@deltares.nl>
2023-03-09 11:03:42 +0100

    Test

--- geolib/models/base_model.py
+++ geolib/models/base_model.py
@@ -81,7 +81,7 @@ class BaseModel(BaseDataClass, abc.ABC):
         # Unsuccessful run
         else:
             error = self.get_error_context()
-            raise CalculationError(process.returncode, error)
+            raise CalculationError(process.returncode, error + " Path: " + str(output_filename.absolute))
 
     def execute_remote(self, endpoint: HttpUrl) -> "BaseModel":
         """Execute a Model on a remote endpoint.
