d5b1656e90170e14db3b22433a37f991eeb06bcf
zantDelta <101396591+zantDelta@users.noreply.github.com>
2023-03-14 10:02:41 +0100

    fix: Remove Earthquake from D-SheetPiling (#107)
    * Remove Earthquake from DSheetPiling python code
    #106 / MSH-4520
    
    * Corrected note in documentation
    #106 / MSH-4520
    
    * removed redundant lines from autoclass to resolve warning in generating documentation
    #106 / MSH-4520

--- geolib/models/dsheetpiling/dsheetpiling_model.py
+++ geolib/models/dsheetpiling/dsheetpiling_model.py
@@ -22,7 +22,6 @@ from .internal import (
     Water,
 )
 from .loads import (
-    Earthquake,
     HorizontalLineLoad,
     Moment,
     NormalForce,
@@ -400,18 +399,18 @@ class DSheetPilingModel(BaseModel):
             HorizontalLineLoad,
             NormalForce,
             SoilDisplacement,
-            Earthquake,
             UniformLoad,
         ],
         stage_id: int,
     ):
-        """Adds other loads of type Moment, HorizontalLineLoad, NormalForce, SoilDisplacement or Earthquake
+        """Adds other loads of type Moment, HorizontalLineLoad, NormalForce, SoilDisplacement or UniformLoad
 
         Args:
-            load: Add a load with the types of Moment, HorizontalLineLoad, NormalForce, SoilDisplacement or Earthquake.
-            Note that SoilDisplacement and Earthquake are only valid for a Pile construction.
+            load: Add a load with the types of Moment, HorizontalLineLoad, NormalForce, SoilDisplacement or UniformLoad.
             stage_id: Load is added to this stage.
 
+        Note: SoilDisplacement and UniformLoad are only valid for a sheetpiling construction.
+        
         Raises:
             ValueError: When non-existing stage_id is passed.
             ValueError: When a verification calculation is selected but duration_type and load_type are not defined for the load.
